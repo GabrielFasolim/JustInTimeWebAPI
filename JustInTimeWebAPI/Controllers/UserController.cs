ba19263590c40a6129e989b23a76f61cdb80c249
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-09-17 21:36:41 -0300

    [add] adicionado appjson eureka e lauchjson eureka e um modelo de user controler

---
+++ JustInTimeWebAPI/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JustInTimeWebAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+/*
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JustInTimeWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly DiscoveryHttpClientHandler _handler;
+
+        public UserController(ILogger<UserController> logger, IDiscoveryClient client)
+        {
+            _logger = logger;
+            _handler = new DiscoveryHttpClientHandler(client);
+        }
+
+        // GET api/user
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient(_handler, false);
+            return await client.GetStringAsync("http://EurekaRegisterExample/api/WeatherForecast");
+        }
+    }
+}
+*/
